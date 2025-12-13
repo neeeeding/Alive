0eaf5fb48392b9aeb529fec4eba8b85a295aa85b
neeeeding <abc01096780866@gmail.com>
2025-12-13 22:54:10 +0900

    Feat : NotInputUI

---
+++ Assets/02Script/UI/Etc/NotInputUI.cs
@@ -0,0 +1,19 @@
+﻿using _02Script.Player;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace _02Script.UI.Etc
+{
+    public class NotInputUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public void OnPointerEnter(PointerEventData pointerEventData)
+        {
+            PlayerInput.Instance.NoInput();
+        }
+
+        public void OnPointerExit(PointerEventData pointerEventData)
+        {
+            PlayerInput.Instance.CanInput();
+        }
+    }
+}
