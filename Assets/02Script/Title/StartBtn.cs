d161b7180a47db3e8830cc19fd1fa13cb0856b79
neeeeding <abc01096780866@gmail.com>
2026-05-06 15:18:33 +0900

    Feat : isis PSD, title Scene

---
+++ Assets/02Script/Title/StartBtn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _02Script.Title
+{
+    public class StartBtn : MonoBehaviour
+    {
+        [SerializeField] private GameObject goStartList;
+        [SerializeField] private string startScene = "AM_House";
+
+        public void StartGame()
+        {
+            SceneManager.LoadScene(startScene);
+        }
+
+        public void GoStartBtn()
+        {
+            goStartList.SetActive(!goStartList.activeSelf);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
