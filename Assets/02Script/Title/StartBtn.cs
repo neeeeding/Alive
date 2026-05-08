d679506e3b8bae05127db161cbfc1509647bee26
neeeeding <abc01096780866@gmail.com>
2026-05-08 22:42:45 +0900

    Fix : StartBtn, image pixels per unit

--- Assets/02Script/Title/StartBtn.cs
+++ Assets/02Script/Title/StartBtn.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using _02Script.Manager;
+using _02Script.SaveData;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace _02Script.Title
@@ -8,6 +10,19 @@ namespace _02Script.Title
         [SerializeField] private GameObject goStartList;
         [SerializeField] private string startScene = "AM_House";
 
+        public void NewGame()
+        {
+            GameSaveData data = new GameSaveData();
+            data.DataReset();
+            data.stat.ResetStat();
+                
+            HouseManager.Instance.PlayerStat = data.stat; //로드
+            
+            PlayerPrefs.SetString(HouseManager.GameSaveFilePath,"");
+            PlayerPrefs.Save();
+            SceneManager.LoadScene(startScene);   
+        }
+        
         public void StartGame()
         {
             SceneManager.LoadScene(startScene);
